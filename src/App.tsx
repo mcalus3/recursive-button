bf44805dcf9e21099011f0c2ad0ca6a248757464
mcalus3 <marek-calus@wp.pl>
2020-02-14 00:20:19 +0100

    Add counter and reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import RecursiveButton from "./RecursiveButton";
+import { useCountState, useCountDispatch } from "./countContext";
 
 const App: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const count = useCountState();
+  const dispatch = useCountDispatch();
+
   return (
     <div className="app">
       <div className="button-container">
-        <RecursiveButton first />
+        <RecursiveButton first clicked={open} setClicked={setOpen} />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            dispatch(-count + 1);
+            setOpen(false);
+          }}
+        >
+          reset
+        </button>
+        <div style={{ color: "white" }}>Elements: {count}</div>
       </div>
     </div>
   );
--- src/RecursiveButton.tsx
+++ src/RecursiveButton.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./RecursiveButton.css";
 import { Howl } from "howler";
+import { useCountDispatch } from "./countContext";
 
 var sound = new Howl({
   src: ["pop.m4a"],
   volume: 0.5
 });
 
-type Props = { first?: boolean };
+type Props = {
+  first?: boolean;
+  clicked?: boolean;
+  setClicked?: (value: boolean) => void;
+};
 
-const RecursiveButton: React.FC<Props> = ({ first }) => {
-  const [clicked, setClicked] = React.useState(false);
+const RecursiveButton: React.FC<Props> = ({
+  first,
+  clicked: clickedProp,
+  setClicked: setClickedProp
+}) => {
+  const [clickedState, setClickedState] = React.useState(false);
+  const dispatch = useCountDispatch();
 
-  return clicked ? (
+  const open = clickedProp !== undefined ? clickedProp : clickedState;
+  const onOpen =
+    setClickedProp !== undefined ? setClickedProp : setClickedState;
+
+  useEffect(() => {
+    if (clickedProp !== undefined) {
+      setClickedState(clickedProp);
+    }
+  }, [clickedProp]);
+
+  return open ? (
     <div className="f-column f-grow">
       <div className="d-flex f-grow">
         <RecursiveButton />
@@ -34,7 +54,8 @@ const RecursiveButton: React.FC<Props> = ({ first }) => {
     <button
       className="r-button f-grow"
       onClick={() => {
-        setClicked(true);
+        dispatch(7);
+        onOpen(true);
         sound.play();
       }}
     >
